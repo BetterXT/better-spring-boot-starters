b6c8627b46c008c0df2232bb043d893af2d9cf48
BetterXT <io.better.chen@gmail.com>
2019-04-26 15:23:30 +0800

    add some dependencies

---
+++ better-jwt-spring-boot-starter/src/main/java/io/better/jwt/config/JwtConfig.java
@@ -0,0 +1,10 @@
+package io.better.jwt.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author better create in 2019-04-26 15:21
+ */
+@Configuration
+public class JwtConfig {
+}
