77ef3667e3d1aacb7da6f55adf40fe52c8a7820a
BetterXT <io.better.chen@gmail.com>
2019-04-26 15:19:19 +0800

    init project

---
+++ test/better-spring-boot-test/src/main/java/io/better/test/TestApplication.java
@@ -0,0 +1,22 @@
+package io.better.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Test application.
+ *
+ * @author better create in 2019-04-26 15:17
+ */
+@SpringBootApplication
+public class TestApplication {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
